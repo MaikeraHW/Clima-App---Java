9533a8382ad19e8b920c55031c78526f158dd8b4
Maicon Henrique Wagner <wagner.maiconhenrique@gmail.com>
2026-04-23 15:01:14 -0300

    Inicio do projeto de criação de um app de acompanhamento climático

---
+++ ClimaApp.java
@@ -0,0 +1,50 @@
+import org.json.JSONObject;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class ClimaApp {
+    public static void main(String[] args) {
+        String cidade;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o nome da cidade: ");
+        cidade = sc.nextLine();
+
+        try {
+            String dadosClimaticos = getDadosClimaticos(cidade);
+
+            if (dadosClimaticos.contains("\"code\":1006")){
+                System.out.println("Cidade não encontrada, tente novamente!");
+            } else {
+                imprimirDadosClimaticos(dadosClimaticos);
+                }
+            
+        } catch (Exception ex) {
+            ex.getMessage();
+        }
+
+    }
+
+// bloco de solicitação de dados pra API
+
+public static String getDadosClimaticos(String cidade) throws Exception { 
+    String apikey = Files.readString( Paths.get("api-key.txt")).trim(); //leitura do arquivo com a APIKey
+
+    String formataNomeCidade = URLEncoder(cidade, StandardCharsets.UTF_8);
+    String apiUrl = "http://api.weatherapi.com/v1/current.json?key=" + apikey + "&q=" + formataNomeCidade; // criação da URL para solicitação (padrão + key + string)
+    HttpRequest request = new HttpRequest.newBuilder() //criando o request
+            .uri(URI.create(apiUrl))
+            .build();
+
+    HttpClient client = HttpClient.newHttpClient();  //criando o requester
+
+    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString()); //armazenando o retorno
+
+    return response.body();
+    };
+}
