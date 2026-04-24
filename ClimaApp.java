f1f5b0b8a83165b75b5dc74ea2ae223a45b95b15
Maicon Henrique Wagner <wagner.maiconhenrique@gmail.com>
2026-04-24 08:17:19 -0300

    Realizado interface pra interação com o usuário e retorno das informações sem ser pelo terminal

--- ClimaApp.java
+++ ClimaApp.java
@@ -1,3 +1,9 @@
+import javafx.application.Application;
+import javafx.geometry.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.stage.*;
 import org.json.JSONObject;
 import java.net.URI;
 import java.net.URLEncoder;
@@ -7,30 +13,68 @@ import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Scanner;
 
-public class ClimaApp {
-    public static void main(String[] args) {
-        String cidade;
+    public class ClimaApp extends Application{
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Digite o nome da cidade: ");
-        cidade = sc.nextLine();
+        @Override
+        public void start(Stage palco){
 
-        try {
-            String dadosClimaticos = getDadosClimaticos(cidade);
-
-            if (dadosClimaticos.contains("\"code\":1006")){
-                System.out.println("Cidade não encontrada, tente novamente!");
-            } else {
-                imprimirDadosClimaticos(dadosClimaticos);
-                }
+            Label cityLabel = new Label("Digite o nome da cidade: ");
+            TextField cityField = new TextField();
+            Button getResult = new Button("Resultado");
+            Label result = new Label();
             
-        } catch (Exception ex) {
-            ex.getMessage();
-        }
 
-    }
+            getResult.setOnAction(e ->{
+                String cityName = cityField.getText();
+
+                try {
+                    String dadosClimaticos = getDadosClimaticos(cityName);
+
+                    if (dadosClimaticos.contains("\"code\":1006")){
+                        result.setText(String.format("Cidade não encontrada, tente novamente!"));
+            } else {
+                JSONObject dadosJson = new JSONObject(dadosClimaticos);
+                JSONObject informacoesAtualizadas = dadosJson.getJSONObject("current");
+
+                String cidade = dadosJson.getJSONObject("location").getString("name");
+                String pais = dadosJson.getJSONObject("location").getString("country");
+                String condicaoTempo = informacoesAtualizadas.getJSONObject("condition").getString("text");
+                int umidade = informacoesAtualizadas.getInt("humidity");
+                float velocidadeVento = informacoesAtualizadas.getFloat("wind_kph");
+                float pressaoatmos = informacoesAtualizadas.getFloat("pressure_mb");
+                float sensacaoTermica = informacoesAtualizadas.getFloat("feelslike_c");
+                float temperaturaAtual = informacoesAtualizadas.getFloat("temp_c");
+
+                String dataHoraString = informacoesAtualizadas.getString("last_updated");
+
+
+                result.setText("Informações Meteorológicas para " + cidade + ", " + pais + 
+                                "\nCondição do tempo: " + condicaoTempo +
+                                "\nTemperatura: " + temperaturaAtual + " ºC" + 
+                                "\nSensação térmica:  " + sensacaoTermica + " ºC" + 
+                                "\nUmidade: " + umidade + " %" +
+                                "\nVelocidade do vento: " + velocidadeVento + " km/H" + 
+                                "\nPressão atmosférica: " + pressaoatmos + "mb" + 
+                                "\nDados atualizado as " + dataHoraString);
+
+                }
+                    
+                } catch (Exception ex) {
+                    ex.getMessage();
+                }
+            });
+
+                VBox vbox = new VBox(10, cityLabel, cityField, getResult, result);
+                vbox.setPadding(new Insets(20));
+
+                Scene cena = new Scene(vbox,350,350);
+
+                palco.setScene(cena);
+                palco.setTitle("App do Clima");
+                palco.show();
+
+        }
 
 // bloco de solicitação de dados pra API
 
@@ -38,7 +82,7 @@ public static String getDadosClimaticos(String cidade) throws Exception {
     String apikey = Files.readString( Paths.get("api-key.txt")).trim(); //leitura do arquivo com a APIKey
 
     String formataNomeCidade = URLEncoder.encode(cidade, StandardCharsets.UTF_8);
-    String apiUrl = "http://api.weatherapi.com/v1/current.json?key=" + apikey + "&q=" + formataNomeCidade; // criação da URL para solicitação (padrão + key + string)
+    String apiUrl = "http://api.weatherapi.com/v1/current.json?key=" + apikey + "&q=" + formataNomeCidade + "&lang=pt"; // criação da URL para solicitação (padrão + key + string)
     HttpRequest request = HttpRequest.newBuilder() //criando o request
             .uri(URI.create(apiUrl))
             .build();
@@ -50,35 +94,8 @@ public static String getDadosClimaticos(String cidade) throws Exception {
     return response.body();
     };
 
-
-    //método para impressão dos dados
-
-    public static void imprimirDadosClimaticos(String dados){
-        //System.out.println("Dados originais JSON obtidos no site da API" + dados);
-        JSONObject dadosJson = new JSONObject(dados);
-        JSONObject informacoesAtualizadas = dadosJson.getJSONObject("current");
-
-        String cidade = dadosJson.getJSONObject("location").getString("name");
-        String pais = dadosJson.getJSONObject("location").getString("country");
-
-        String condicaoTempo = informacoesAtualizadas.getJSONObject("condition").getString("text");
-        int umidade = informacoesAtualizadas.getInt("humidity");
-        float velocidadeVento = informacoesAtualizadas.getFloat("wind_kph");
-        float pressaoatmos = informacoesAtualizadas.getFloat("pressure_mb");
-        float sensacaoTermica = informacoesAtualizadas.getFloat("feelslike_c");
-        float temperaturaAtual = informacoesAtualizadas.getFloat("temp_c");
-
-        String dataHoraString = informacoesAtualizadas.getString("last_updated");
-
-        System.out.println("Condição do tempo: " + condicaoTempo);
-        System.out.println("Informações Meteorológicas para " + cidade + ", " + pais);
-        System.out.println("Temperatura: " + temperaturaAtual + " ºC");
-        System.out.println("Sensação térmica:  " + sensacaoTermica + " ºC");
-        System.out.println("Umidade: " + umidade);
-        System.out.println("Velocidade do vento: " + velocidadeVento + " Km/H");
-        System.out.println("Pressão atmosférica: " + pressaoatmos + "mb");
-        System.out.println("Dados atualizado as " + dataHoraString);
+    public static void main(String[] args) {  
+        launch(args);
 
     }
-
 }
